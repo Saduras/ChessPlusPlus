14e576eebbb667e899e7ea6f7ed81f59dbd9d06a
David Speck <david@saduras.de>
2017-12-10 15:06:10 +0100

    Fix wstring in TestUtils

--- ChessPlusPlus-Tests/TestUtils.cpp
+++ ChessPlusPlus-Tests/TestUtils.cpp
@@ -1,4 +1,5 @@
 #include "stdafx.h"
+#include "CppUnitTest.h"
 #include "TestUtils.h"
 #include <string>
 #include <locale>
@@ -11,28 +12,23 @@ bool containsPosition(std::vector<Position> list, Position position)
 	return std::find(list.begin(), list.end(), position) != list.end();
 }
 
-const wchar_t* toWideStr(std::string str)
-{
-	std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
-	std::wstring wstring =  converter.from_bytes(str);
-	return wstring.c_str();
-}
-
 void TestUtils::AssertPositions(std::vector<Position> positions, std::vector<bool> exspectedPositions)
 {
 	for (int x = 0; x < 8; x++)
 	{
 		for (int y = 0; y < 8; y++)
 		{
-			int index = x + y * 8;
+			int index = x + (7 - y) * 8;
 			Position pos{ x, y };
 			if (exspectedPositions[index])
 			{
-				Assert::IsTrue(containsPosition(positions, pos), toWideStr("Missing position " + Position::toString(pos) ) );
+				std::wstring message{ L"Missing position " + std::to_wstring(pos.x) + L", " + std::to_wstring(pos.y) };
+				Assert::IsTrue(containsPosition(positions, pos), message.c_str());
 			}
 			else
 			{
-				Assert::IsFalse(containsPosition(positions, pos), toWideStr("Invalid position " + Position::toString(pos) ) );
+				std::wstring message{ L"Invalid position " + std::to_wstring(pos.x) + L", " + std::to_wstring(pos.y) };
+				Assert::IsFalse(containsPosition(positions, pos), message.c_str());
 			}
 		}
 	}
