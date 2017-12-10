700ae871312423fddbea53353f25822d07724de2
David Speck <david@saduras.de>
2017-12-10 13:59:02 +0100

    Fix Position += operator overload

--- ChessPlusPlus/Position.h
+++ ChessPlusPlus/Position.h
@@ -9,11 +9,12 @@ struct Position
 
 	static bool isOnBoard(Position pos);
 	static std::string toString(Position pos);
+
+	struct Position& operator+=(const Position& rhs) { x += rhs.x; y += rhs.y; return *this; }
+	struct Position& operator-=(const Position& rhs) { x -= rhs.x; y -= rhs.y; return *this; }
 };
 
 inline bool operator==(const Position& lhs, const Position& rhs) { return lhs.x == rhs.x && lhs.y == rhs.y; }
 inline bool operator!=(const Position& lhs, const Position& rhs) { return !(lhs == rhs); }
 inline Position operator+(const Position& lhs, const Position& rhs) { return Position{ lhs.x + rhs.x, lhs.y + rhs.y }; }
-inline Position operator+=(const Position& lhs, const Position& rhs) { return lhs + rhs; }
 inline Position operator-(const Position& lhs, const Position& rhs) { return Position{ lhs.x - rhs.x, lhs.y - rhs.y }; }
-inline Position operator-=(const Position& lhs, const Position& rhs) { return lhs - rhs; }
