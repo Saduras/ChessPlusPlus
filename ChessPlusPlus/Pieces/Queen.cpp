145ed65115a971107da55e96df1f23c7ddab9281
David Speck <david@saduras.de>
2017-12-10 17:04:54 +0100

    Fix queen move patterns

--- ChessPlusPlus/Pieces/Queen.cpp
+++ ChessPlusPlus/Pieces/Queen.cpp
@@ -6,7 +6,7 @@ static Position offsets[8]{
 	// straigth lines
 	Position{ 1, 0 },
 	Position{ 0, -1 },
-	Position{ 1, -0 },
+	Position{ -1, 0 },
 	Position{ 0, 1 },
 	// diagonales
 	Position{ 1, 1 },
@@ -28,7 +28,7 @@ std::vector<Position> Queen::getMovesFor(Position pos, Board &board)
 			auto other = board.getPieceAt(newPos);
 			if (other)
 			{
-				if (other->getColor() == this->getColor())
+				if (other->getColor() != this->getColor())
 					vector.push_back(newPos);
 
 				break;
