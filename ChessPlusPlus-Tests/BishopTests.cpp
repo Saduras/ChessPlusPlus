f6249d9b79187d3ba65a7a2f7b8de58f6e2d54e1
David Speck <david@saduras.de>
2017-12-10 15:41:24 +0100

    Complete bishop test suit

--- ChessPlusPlus-Tests/BishopTests.cpp
+++ ChessPlusPlus-Tests/BishopTests.cpp
@@ -12,7 +12,7 @@ namespace ChessPlusPlusTests
 	{
 	public:
 		
-		TEST_METHOD(ValidMovesTest)
+		TEST_METHOD(BishopFreeBoardTest)
 		{
 			// Arrange
 			Board board{};
@@ -36,5 +36,82 @@ namespace ChessPlusPlusTests
 			});
 		}
 
+		TEST_METHOD(BishopCornerTest)
+		{
+			// Arrange
+			Board board{};
+			Bishop *piece = new Bishop{ Color::WHITE };
+			Position pos{ 7,7 };
+			board.placePieceAt(piece, pos);
+
+			// Act
+			auto validPositions = piece->getMovesFor(pos, board);
+
+			// Assert
+			TestUtils::AssertPositions(validPositions, {
+				0,0,0,0,0,0,0,0,
+				0,0,0,0,0,0,1,0,
+				0,0,0,0,0,1,0,0,
+				0,0,0,0,1,0,0,0,
+				0,0,0,1,0,0,0,0,
+				0,0,1,0,0,0,0,0,
+				0,1,0,0,0,0,0,0,
+				1,0,0,0,0,0,0,0,
+			});
+		}
+
+		TEST_METHOD(BishopAttackTest)
+		{
+			// Arrange
+			Board board{};
+			Bishop *whitePiece = new Bishop{ Color::WHITE };
+			Position whitePos{ 3,3 };
+			Bishop *blackPiece = new Bishop{ Color::BLACK };
+			Position blackPos{ 5,5 };
+			board.placePieceAt(whitePiece, whitePos);
+			board.placePieceAt(blackPiece, blackPos);
+
+			// Act
+			auto validPositions = whitePiece->getMovesFor(whitePos, board);
+
+			// Assert
+			TestUtils::AssertPositions(validPositions, {
+				0,0,0,0,0,0,0,0,
+				1,0,0,0,0,0,0,0,
+				0,1,0,0,0,1,0,0,
+				0,0,1,0,1,0,0,0,
+				0,0,0,0,0,0,0,0,
+				0,0,1,0,1,0,0,0,
+				0,1,0,0,0,1,0,0,
+				1,0,0,0,0,0,1,0,
+			});
+		}
+
+		TEST_METHOD(BishopBlockTest)
+		{
+			// Arrange
+			Board board{};
+			Bishop *whitePiece = new Bishop{ Color::WHITE };
+			Position whitePos{ 3,3 };
+			Bishop *otherPiece = new Bishop{ Color::WHITE };
+			Position otherPos{ 5,5 };
+			board.placePieceAt(whitePiece, whitePos);
+			board.placePieceAt(otherPiece, otherPos);
+
+			// Act
+			auto validPositions = whitePiece->getMovesFor(whitePos, board);
+
+			// Assert
+			TestUtils::AssertPositions(validPositions, {
+				0,0,0,0,0,0,0,0,
+				1,0,0,0,0,0,0,0,
+				0,1,0,0,0,0,0,0,
+				0,0,1,0,1,0,0,0,
+				0,0,0,0,0,0,0,0,
+				0,0,1,0,1,0,0,0,
+				0,1,0,0,0,1,0,0,
+				1,0,0,0,0,0,1,0,
+			});
+		}
 	};
 }
