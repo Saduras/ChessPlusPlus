48d7b67528692d6ad2e49c8cabf7182a1ace33de
David Speck <david@saduras.de>
2017-12-12 22:07:38 +0100

    Setup SFML

--- ChessPlusPlus/ChessPlusPlus.cpp
+++ ChessPlusPlus/ChessPlusPlus.cpp
@@ -4,13 +4,37 @@
 #include "stdafx.h"
 #include "ChessPlusPlus.h"
 #include "Match.h"
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-	Match match{};
-	match.start();
+	//Match match{};
+	//match.start();
 
-	system("pause");
+	//system("pause");
+
+	sf::Texture t1;
+	t1.loadFromFile("images/board.png");
+
+	sf::Sprite s{ t1 };
+
+	sf::RenderWindow window{ sf::VideoMode{ 800, 800 }, "ChessPlusPlus" };
+	window.setFramerateLimit(30);
+	
+	while (window.isOpen())
+	{
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		window.clear();
+		window.draw(s);
+		window.display();
+	}
+
 	return 0;
 }
 
---
+++ ChessPlusPlus/WindowView.h
@@ -0,0 +1,8 @@
+#pragma once
+class WindowView
+{
+public:
+	WindowView();
+	~WindowView();
+};
+
