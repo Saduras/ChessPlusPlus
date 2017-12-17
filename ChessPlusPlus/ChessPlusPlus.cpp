273f987ae050d1574d68f49dcc8c1677246720d6
David Speck <david@saduras.de>
2017-12-17 11:24:54 +0100

    Drag & drop

--- ChessPlusPlus/ChessPlusPlus.cpp
+++ ChessPlusPlus/ChessPlusPlus.cpp
@@ -6,6 +6,8 @@
 #include "Match.h"
 #include <SFML/Graphics.hpp>
 
+using namespace sf;
+
 int main()
 {
 	//Match match{};
@@ -13,23 +15,46 @@ int main()
 
 	//system("pause");
 
-	sf::Texture t1;
-	t1.loadFromFile("images/board.png");
+	Texture t1;
+	t1.loadFromFile("images/pieces.png");
 
-	sf::Sprite s{ t1 };
+	Sprite s{ t1 };
 
-	sf::RenderWindow window{ sf::VideoMode{ 800, 800 }, "ChessPlusPlus" };
+	RenderWindow window{ VideoMode{ 800, 800 }, "ChessPlusPlus" };
 	window.setFramerateLimit(30);
 	
+	bool isMove = false;
+	int dx = 0;
+	int dy = 0;
 	while (window.isOpen())
 	{
-		sf::Event event;
+		Event event;
+
 		while (window.pollEvent(event))
 		{
-			if (event.type == sf::Event::Closed)
+			Vector2i pos = Mouse::getPosition(window);
+			if (event.type == Event::Closed)
 				window.close();
+
+			// drag and drop
+			if (event.key.code == Mouse::Left)
+			{
+				if (event.type == Event::MouseButtonPressed && s.getGlobalBounds().contains(pos.x, pos.y))
+				{
+					isMove = true;
+					dx = pos.x - s.getPosition().x;
+					dy = pos.y - s.getPosition().y;
+				}
+
+				if (event.type == Event::MouseButtonReleased)
+					isMove = false;
+			}
+
+			if (isMove)
+				s.setPosition(pos.x - dx, pos.y - dy);
 		}
 
+		// draw
 		window.clear();
 		window.draw(s);
 		window.display();
